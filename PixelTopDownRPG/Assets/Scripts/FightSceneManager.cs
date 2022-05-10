9cd642a32c620488807a156132a82dbd5275f358
marasCocuk <ahmeteditor64@gmail.com>
2022-05-10 18:21:57 +0300

    l

--- PixelTopDownRPG/Assets/Scripts/FightSceneManager.cs
+++ PixelTopDownRPG/Assets/Scripts/FightSceneManager.cs
@@ -12,16 +12,32 @@ namespace GameMaster
         GameObject player;
         List<GameObject> enemies;
 
+        private void Start()
+        {
+            CreatePlayer();
+        }
 
 
 
         void CreatePlayer()
         {
-            player = new GameObject();
+            player = new GameObject("Player");
             player.AddComponent<Player>();
+            player.AddComponent<SpriteRenderer>();
+            player.tag = "Player";
 
         }
-
+        void CreateEnemies(List<Enemy> enemies)
+        {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                GameObject newEnemy = new GameObject("Enemy");
+                newEnemy.AddComponent<Enemy>();
+                newEnemy.AddComponent<SpriteRenderer>();
+                newEnemy.tag = "Player";
+                this.enemies.Add();
+            }
+        }
 
     }
 
