8f0c7a42b673d89e898319255f195e80e085d94f
Ömer Faruk BARAN <86892804+BlackRose9111@users.noreply.github.com>
2022-05-17 11:14:33 +0300

    Character Update

--- PixelTopDownRPG/Assets/Scripts/CharacterTypes/CharacterDetails.cs
+++ PixelTopDownRPG/Assets/Scripts/CharacterTypes/CharacterDetails.cs
@@ -6,29 +6,64 @@ using Actions.Attacks;
 namespace CharacterTypes
 {
     /// <summary>
-    /// A class that is going to keep all character properties. 
+    /// A class that is going to keep all character properties.
     /// </summary>
     public abstract class CharacterDetails : MonoBehaviour
     {
+        /// <summary>
+        ///Important Values
+        /// </summary>
+
+        //Health
+
         public int baseHealth = 500;
         public int maxHealth;
         public int currentHealth;
+
+        //Stamina
+
+        public int baseStamina = 100;
         public int maxStamina;
         public int currentStamina;
+
+        //Magic
+
+        public int baseMagic = 0;
+        public int maxMagic;
+        public int currentMagic;
+
+        //Experience
+
+        public int basisXpForRequiredToLevelUp = 100;
         public int level;
         public int xp;
+        public int requiredXp;
 
         /// <summary>
         /// Stats
         /// </summary>
 
-        public int strength; /* canï¿½ ve fizksel saldï¿½rï¿½larï¿½ etkileyen stat*/
-        public int dexterity;
-        public int wisdom;
+        public int strength; /*Physical attack stat, affects stamina*/
+        public int vitality; /* health and defensive stat*/
+        public int intelligence; /*Magic point and magic attack.*/
+
+        /// <summary>
+        /// Actions
+        /// </summary>
+
+        private List<Attack> attacks;
 
         public void levelUp(/*prototip*/)
         {
-            UpdateHealth();
+            if (xp >= requiredXp)
+            {
+                level++;
+                xp = xp - requiredXp;
+                UpdateMaxXp();
+                UpdateHealth();
+                UpdateStamina();
+                UpdateMagic();
+            }
         }
 
         /// <summary>
@@ -36,9 +71,36 @@ namespace CharacterTypes
         /// </summary>
         public void UpdateHealth()
         {
-            maxHealth = baseHealth + 500 * level + 100 * strength;
+            maxHealth = baseHealth + (100 * level) + (500 * vitality);
         }
 
-        private List<Attack> attacks;
+        /// <summary>
+        /// Update the max stamina data
+        /// </summary>
+        public void UpdateStamina()
+        {
+            maxStamina = baseStamina + (100 * level) + (500 * strength);
+        }
+
+        /// <summary>
+        /// Update the max magic data
+        /// </summary>
+        public void UpdateMagic()
+        {
+            maxMagic = baseMagic + (100 * level) + (500 * intelligence);
+        }
+
+        /// <summary>
+        /// Max xp
+        /// </summary>
+        public void UpdateMaxXp()
+        {
+            requiredXp = basisXpForRequiredToLevelUp + (level * level * 1000);
+        }
+
+        public void Attack(Attack at)
+        {
+            /*Saldýrý yap*/
+        }
     }
 }
