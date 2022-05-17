b63a8eb182e1aeee328ea767741f99320eb15834
marasCocuk <ahmeteditor64@gmail.com>
2022-05-17 16:21:33 +0300

    sa

---
+++ PixelTopDownRPG/Assets/Scripts/InventorySystem/Outfit/Gloves.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace InventorySystem.Outfit
+{
+    public class Gloves : Outfit
+    {
+
+    }
+}
---
+++ PixelTopDownRPG/Assets/Scripts/InventorySystem/Outfit/Outfit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace InventorySystem.Outfit
+{
+    public class Outfit : Item
+    {
+        public Outfit()
+        {
+
+        }
+    }
+}
+
