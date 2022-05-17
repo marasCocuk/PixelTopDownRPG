f4d91ef8df016cad18186ae496b9ec06b567f8f7
marasCocuk <ahmeteditor64@gmail.com>
2022-05-17 13:36:28 +0300

    asdf
    asd

--- PixelTopDownRPG/Assets/Scripts/CharacterTypes/EnemyDetails.cs
+++ PixelTopDownRPG/Assets/Scripts/CharacterTypes/EnemyDetails.cs
@@ -10,5 +10,17 @@ namespace CharacterTypes
     /// </summary>
     public class EnemyDetails : CharacterDetails
     {
+        public EnemyDetails(int level, int strength, int vitality, int intelligence, int baseHealth = 500, int baseStamina = 100, int baseMagic = 0, int basisXpForRequiredToLevelUp = 100) : base(level, strength, vitality, intelligence, baseHealth = 500, baseStamina = 100, baseMagic = 0, basisXpForRequiredToLevelUp = 100)
+        {
+            this.level = level;
+            this.strength = strength;
+            this.vitality = vitality;
+            this.intelligence = intelligence;
+            this.baseHealth = baseHealth;
+            this.baseStamina = baseStamina;
+            this.baseMagic = baseMagic;
+            this.basisXpForRequiredToLevelUp = basisXpForRequiredToLevelUp;
+        }
+
     }
 }
